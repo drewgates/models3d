8628d6a6701d503e7d736d42e2dc2ec4ba5f331f
Drew Gates <7493966+drewgates@users.noreply.github.com>
2018-12-08 17:26:33 -0500

    remove ground plane

--- webgl.php
+++ webgl.php
@@ -70,16 +70,15 @@
 				scene.background = new THREE.Color( 0x72645b );
 				scene.fog = new THREE.Fog( 0x72645b, 2, 15 );
 
-				// Ground
-				var plane = new THREE.Mesh(
-					new THREE.PlaneBufferGeometry( 40, 40 ),
-					new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
-				);
-				plane.rotation.x = -Math.PI/2;
-				plane.position.y = -0.5;
-				scene.add( plane );
-
-				plane.receiveShadow = true;
+				// Ground - uncomment the next eight lines if you want a ground plane below the models.
+				// var plane = new THREE.Mesh(
+				//	new THREE.PlaneBufferGeometry( 40, 40 ),
+				//	new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
+				//);
+				//plane.rotation.x = -Math.PI/2;
+				//plane.position.y = -0.5;
+				//scene.add( plane );
+				//plane.receiveShadow = true;
 
 
 				// ASCII file
